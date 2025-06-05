cd717b0ade6159bc5d47618aced4dae3f5b12c06
Igor Kolisnyk <moonbosszzz@gmail.com>
2025-06-05 21:20:18 +0200

    The solution of the task

--- taxi/models.py
+++ taxi/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(unique=True)
-    country = models.CharField()
+    name = models.CharField(unique=True, max_length=255)
+    country = models.CharField(max_length=255)
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(unique=True)
+    license_number = models.CharField(unique=True, max_length=255)
 
 
 class Car(models.Model):
-    model = models.CharField()
+    model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     driver = models.ManyToManyField(Driver)
